27b1d1c9fc2cb6e493d830c6e04324eeacd482b8
Hemantkoli14321 <hemantkoli14321@gmail.com>
2021-04-09 17:14:12 +0530

    Update TakeSurvey.js

---
+++ src/survey/TakeSurvey.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TakeSurvey = props => {
+    return (
+        <div>
+            <p>Take Survey</p>            
+        </div>
+    );
+};
+ export default TakeSurvey;
